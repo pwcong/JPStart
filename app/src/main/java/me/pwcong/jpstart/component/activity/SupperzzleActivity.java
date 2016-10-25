46a3cddace26adaabdd2f65cbaa42c9cd734ba4f
Pwcong <pwcong@foxmail.com>
2016-10-25 12:26:56 +0800

    基本实现我是谁小游戏#2

--- app/src/main/java/me/pwcong/jpstart/component/activity/SupperzzleActivity.java
+++ app/src/main/java/me/pwcong/jpstart/component/activity/SupperzzleActivity.java
@@ -2,7 +2,10 @@ package me.pwcong.jpstart.component.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
+import butterknife.BindView;
 import me.pwcong.jpstart.R;
 
 /**
@@ -10,6 +13,10 @@ import me.pwcong.jpstart.R;
  */
 
 public class SupperzzleActivity extends BaseActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+
     @Override
     protected int getViewId() {
         return R.layout.activity_supperzzle;
@@ -18,7 +25,23 @@ public class SupperzzleActivity extends BaseActivity {
     @Override
     protected void initVariable(@Nullable Bundle savedInstanceState) {
 
+        initToolbar();
     }
+
+    private void initToolbar(){
+        mToolbar.setTitle(R.string.supperzzle);
+        mToolbar.setNavigationIcon(R.drawable.ic_chevron_left_white_24dp);
+        setSupportActionBar(mToolbar);
+
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+    }
+
 
     @Override
     protected void doAction() {
